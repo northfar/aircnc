36ddbd0eb54849d5d012d517a9608388008f5b48
northfar <north.farias@gmail.com>
2019-11-21 20:00:31 -0400

    Frontend da aplicação AirCnC

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Routes from './routes';
+import logo from './assets/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div className="container">
+   <img src={logo} alt=""/>
+      <div className="content">
+          <Routes />
+      </div>
+   </div>
+  );
+}
+
+export default App;
